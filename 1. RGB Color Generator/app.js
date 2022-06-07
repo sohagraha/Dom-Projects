8482ac53f16c397679759a3f4668b4e789ddfeb1
sohagraha <sohagrahabd@gmail.com>
2022-06-07 09:04:10 +0600

    rgb color generate and copy

--- 1. RGB Color Generator/app.js
+++ 1. RGB Color Generator/app.js
@@ -12,7 +12,9 @@ function main() {
 	const autobtnstop = document.getElementById('change-btn-auto-stop');
 
 	let txtcolor = document.getElementById('text-color');
+	let txtcolorrgb = document.getElementById('text-color-rgb');
 	let copyBtn = document.getElementById('copy_hex');
+	let copyBtn2 = document.getElementById('copy2');
 
 	autobtnstop.style.display = "none";
 
@@ -20,6 +22,7 @@ function main() {
 		const bgColor = generateRGBColor();
 		root.style.backgroundColor = bgColor;
 		txtcolor.innerText = bgColor;
+		txtcolorrgb.value = bgColor;
 	});
 
 	autobtn.addEventListener('click', function () {
@@ -28,9 +31,10 @@ function main() {
 		autobtnstop.style.display = "block";
 
 		var intervalId = window.setInterval(function () {
-			const bgColor = generateRGBColor();
+			let bgColor = generateRGBColor();
 			root.style.backgroundColor = bgColor;
 			txtcolor.innerText = bgColor;
+			txtcolorrgb.value = bgColor;
 		}, 5000);
 
 		autobtnstop.addEventListener('click', function () {
@@ -59,12 +63,22 @@ function main() {
 		}
 	});
 
+	copyBtn2.addEventListener('click', () => {
+		let value = txtcolorrgb.value;
+		if (div !== null) {
+			div.remove();
+			div = null;
+		}
+		generateToastMessage(`${value} copied !!!`)
+	})
+
 	document.getElementById("output").addEventListener('keyup', (e) => {
 		let color = e.target.value;
 		output.value = color.toUpperCase();
 		color = `#${color}`;
 		if (color && isValidHex(color)) {
 			root.style.backgroundColor = color;
+			hexToRGB(color);
 		}
 	})
 }
@@ -116,5 +130,16 @@ const isValidHex = (color) => {
 	}
 	color = color.substring(1);
 	return /^[0-9A-Fa-f]{6}/i.test(color);
+}
+
+const hexToRGB = (color) => {
+	color = color.substring(1);
+	// console.log(color);
+	let r, g, b;
+	r = parseInt(color.slice(0, 2), 16);
+	g = parseInt(color.slice(2, 4), 16);
+	b = parseInt(color.slice(4, 6), 16);
+	document.getElementById('text-color').innerText = `rgb(${r},${g}.${b})`;
+	document.getElementById('text-color-rgb').value = `rgb(${r},${g}.${b})`
 }
 
