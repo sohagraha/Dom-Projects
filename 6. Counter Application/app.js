255b5c22beedc549f9ef030f4a95ef98ecbce31b
sohagraha <sohagrahabd@gmail.com>
2022-06-12 22:40:54 +0600

    some change

--- 6. Counter Application/app.js
+++ 6. Counter Application/app.js
@@ -48,3 +48,20 @@ decBtn.addEventListener('click', () => {
     counterTxt.innerText = count;
 })
 
+
+let test = () => {
+    let arr = ['start', 1, 2, 3, null, false, "3rd", null, 4, 5, '', 'text', 6, 7];
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] !== 'number') {
+            count++;
+            for (let j = i; j < arr.length - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            i--;
+        }
+    }
+    arr.length -= count;
+    console.log(arr)
+}
+test();
